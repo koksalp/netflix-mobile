0722e4de2789d88dbeec8b0e3bdc8e53bca5fcb8
Alp Köksal <72392963+koksalp@users.noreply.github.com>
2023-04-29 21:19:01 +0300

    updated MainScreen.js

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,30 +1,45 @@
 import { View, StyleSheet, ScrollView, Text } from "react-native";
 
-import ImageContainer from "../components/ImageContainer"; 
-import { movieImageUrl,seriesImageUrl , imageHeightMain, imageWidthMain } from "../helpers/constants";
+import ImageContainer from "../components/ImageContainer";
+import {
+  movieImageUrl,
+  seriesImageUrl,
+  imageHeightMain,
+  imageWidthMain,
+} from "../helpers/constants";
 
-export default function MainScreen() { 
-
-  return ( 
+export default function MainScreen() {
+  return (
     <ScrollView>
       <View style={styles.container}>
-          <ImageContainer width={imageWidthMain} height={imageHeightMain} imageUrl={movieImageUrl} onPressFn={() => console.log("movie")} text="Film"/>
-          <ImageContainer width={imageWidthMain} height={imageHeightMain} imageUrl={seriesImageUrl} onPressFn={() => console.log("series")} text="Dizi" /> 
-      </View> 
+        <ImageContainer
+          width={imageWidthMain}
+          height={imageHeightMain}
+          imageUrl={movieImageUrl}
+          onPressFn={() => console.log("movie")}
+          text="Film"
+        />
+        <ImageContainer
+          width={imageWidthMain}
+          height={imageHeightMain}
+          imageUrl={seriesImageUrl}
+          onPressFn={() => console.log("series")}
+          text="Dizi"
+        />
+      </View>
     </ScrollView>
   );
-} 
+}
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: "center", 
-      paddingVertical: 16, 
-    }, 
-    text: {
-        fontSize: 24, 
-    }
-  });
-  
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 16,
+  },
+  text: {
+    fontSize: 24,
+  },
+});
