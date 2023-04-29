86a6ba0ef7a64d4124abf2140f68a0b199a2b65b
Alp Köksal <72392963+koksalp@users.noreply.github.com>
2023-04-29 21:15:16 +0300

    Add files via upload

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,21 +1,30 @@
 import { View, StyleSheet, ScrollView, Text } from "react-native";
 
-export default function MainScreen() {
-  return (
+import ImageContainer from "../components/ImageContainer"; 
+import { movieImageUrl,seriesImageUrl , imageHeightMain, imageWidthMain } from "../helpers/constants";
+
+export default function MainScreen() { 
+
+  return ( 
     <ScrollView>
       <View style={styles.container}>
-        <Text>Main</Text>
-      </View>
+          <ImageContainer width={imageWidthMain} height={imageHeightMain} imageUrl={movieImageUrl} onPressFn={() => console.log("movie")} text="Film"/>
+          <ImageContainer width={imageWidthMain} height={imageHeightMain} imageUrl={seriesImageUrl} onPressFn={() => console.log("series")} text="Dizi" /> 
+      </View> 
     </ScrollView>
   );
-}
+} 
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    paddingVertical: 16,
-  },
-});
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: "center", 
+      paddingVertical: 16, 
+    }, 
+    text: {
+        fontSize: 24, 
+    }
+  });
+  
