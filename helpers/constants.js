603463558c1942e69486b8e115be3ca904ab5018
Alp Köksal <72392963+koksalp@users.noreply.github.com>
2023-04-30 00:34:01 +0300

    updated constants.js

--- helpers/constants.js
+++ helpers/constants.js
@@ -2,3 +2,6 @@ export const movieImageUrl = "https://upload.wikimedia.org/wikipedia/en/1/12/Jum
 export const seriesImageUrl = "https://upload.wikimedia.org/wikipedia/tr/thumb/0/02/The_Witcher_afi%C5%9F.jpg/640px-The_Witcher_afi%C5%9F.jpg"; 
 export const imageHeightMain= 200; 
 export const imageWidthMain = 200 ; 
+export const imageHeightListing = 150; 
+export const imageWidthListing = 150;  
+export const initialProgramLimit = 18; 
