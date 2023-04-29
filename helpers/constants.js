cb08b949d5a88f347935d04db578e6d7fc1eddf6
Alp Köksal <72392963+koksalp@users.noreply.github.com>
2023-04-29 21:14:12 +0300

    created constants.js file under helpers folder

---
+++ helpers/constants.js
@@ -0,0 +1,4 @@
+export const movieImageUrl = "https://upload.wikimedia.org/wikipedia/en/1/12/Jumanji_-The_Next_Level_film_poster.jpg"; 
+export const seriesImageUrl = "https://upload.wikimedia.org/wikipedia/tr/thumb/0/02/The_Witcher_afi%C5%9F.jpg/640px-The_Witcher_afi%C5%9F.jpg"; 
+export const imageHeightMain= 200; 
+export const imageWidthMain = 200 ; 
