bd94678b9568f29afb752deff767eca9e92e8cb3
Alp Köksal <72392963+koksalp@users.noreply.github.com>
2023-04-30 00:31:27 +0300

    updated MainScreen.js

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,45 +1,36 @@
 import { View, StyleSheet, ScrollView, Text } from "react-native";
 
-import ImageContainer from "../components/ImageContainer";
-import {
-  movieImageUrl,
-  seriesImageUrl,
-  imageHeightMain,
-  imageWidthMain,
-} from "../helpers/constants";
+import ImageContainer from "../components/ImageContainer"; 
+import { movieImageUrl,seriesImageUrl , imageHeightMain, imageWidthMain } from "../helpers/constants";
 
-export default function MainScreen() {
-  return (
+export default function MainScreen({navigation}) { 
+
+  function handlePress(programType) {
+    navigation.navigate("ListingScreen", {
+      programType: programType, 
+    }); 
+  } 
+
+  return ( 
     <ScrollView>
       <View style={styles.container}>
-        <ImageContainer
-          width={imageWidthMain}
-          height={imageHeightMain}
-          imageUrl={movieImageUrl}
-          onPressFn={() => console.log("movie")}
-          text="Film"
-        />
-        <ImageContainer
-          width={imageWidthMain}
-          height={imageHeightMain}
-          imageUrl={seriesImageUrl}
-          onPressFn={() => console.log("series")}
-          text="Dizi"
-        />
-      </View>
+          <ImageContainer width={imageWidthMain} height={imageHeightMain} imageUrl={movieImageUrl} onPressFn={() => handlePress("movie")} text="Film"/>
+          <ImageContainer width={imageWidthMain} height={imageHeightMain} imageUrl={seriesImageUrl} onPressFn={() => handlePress("series")} text="Dizi" /> 
+      </View> 
     </ScrollView>
   );
-}
+} 
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    paddingVertical: 16,
-  },
-  text: {
-    fontSize: 24,
-  },
-});
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: "center", 
+      paddingVertical: 16, 
+    }, 
+    text: {
+        fontSize: 24, 
+    }
+  });
+  
