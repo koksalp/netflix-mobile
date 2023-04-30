b0e5fa7c63c004f09c21136efe8b1a254d7cd5f3
Alp Köksal <72392963+koksalp@users.noreply.github.com>
2023-04-30 15:41:18 +0300

    added comments

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,36 +1,62 @@
-import { View, StyleSheet, ScrollView, Text } from "react-native";
+import { View, StyleSheet, ScrollView } from "react-native";
 
-import ImageContainer from "../components/ImageContainer"; 
-import { movieImageUrl,seriesImageUrl , imageHeightMain, imageWidthMain } from "../helpers/constants";
+import ImageContainer from "../components/ImageContainer";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import {
+  movieImageUrl,
+  seriesImageUrl,
+  imageHeightMain,
+  imageWidthMain,
+} from "../helpers/constants";
 
-export default function MainScreen({navigation}) { 
-
+// Main screen component
+// users can choose between 2 categories
+// and get navigated into listing screen
+export default function MainScreen({ navigation }) {
+  // navigate into listing screen and pass
+  // program type selected by user as argument
   function handlePress(programType) {
     navigation.navigate("ListingScreen", {
-      programType: programType, 
-    }); 
-  } 
+      programType: programType,
+    });
+  }
 
-  return ( 
+  // return a scrollable screen where users can select a category
+  // there is a header element on the top and footer on the bottom
+  return (
     <ScrollView>
+      <Header />
       <View style={styles.container}>
-          <ImageContainer width={imageWidthMain} height={imageHeightMain} imageUrl={movieImageUrl} onPressFn={() => handlePress("movie")} text="Film"/>
-          <ImageContainer width={imageWidthMain} height={imageHeightMain} imageUrl={seriesImageUrl} onPressFn={() => handlePress("series")} text="Dizi" /> 
-      </View> 
+        <ImageContainer
+          width={imageWidthMain}
+          height={imageHeightMain}
+          imageUrl={movieImageUrl}
+          onPressFn={() => handlePress("movie")}
+          text="Film"
+        />
+        <ImageContainer
+          width={imageWidthMain}
+          height={imageHeightMain}
+          imageUrl={seriesImageUrl}
+          onPressFn={() => handlePress("series")}
+          text="Dizi"
+        />
+      </View>
+      <Footer />
     </ScrollView>
   );
-} 
+}
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: "center", 
-      paddingVertical: 16, 
-    }, 
-    text: {
-        fontSize: 24, 
-    }
-  });
-  
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 16,
+  },
+  text: {
+    fontSize: 24,
+  },
+});
