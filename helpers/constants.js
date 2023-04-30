376a8af87c08a2ae5748c849b225048e36056827
Alp Köksal <72392963+koksalp@users.noreply.github.com>
2023-04-30 16:37:11 +0300

    added alertMessage and comments

--- helpers/constants.js
+++ helpers/constants.js
@@ -1,10 +1,20 @@
+// links to images of categories (movies/TV series) 
 export const movieImageUrl = "https://upload.wikimedia.org/wikipedia/en/1/12/Jumanji_-The_Next_Level_film_poster.jpg"; 
 export const seriesImageUrl = "https://upload.wikimedia.org/wikipedia/tr/thumb/0/02/The_Witcher_afi%C5%9F.jpg/640px-The_Witcher_afi%C5%9F.jpg"; 
+
+// width and height of the images in main screen
 export const imageHeightMain= 200; 
 export const imageWidthMain = 200 ; 
+
+// width and height of the images in listing screen
 export const imageHeightListing = 150; 
 export const imageWidthListing = 150;  
+
+// number of programs displayed when no filters 
+// from dropdown menu selected 
 export const initialProgramLimit = 18; 
+
+// all filtering options 
 export const filterOptions = [
     { 
         value: "Yeniye Göre Sırala", 
@@ -23,3 +33,6 @@ export const filterOptions = [
         key: "random" 
     }, 
 ] 
+
+// alert message 
+export const alertMessage = "Bu bir deneme uygulaması olduğu için tıkladığınız yerin bir işlevi yoktur."; 
