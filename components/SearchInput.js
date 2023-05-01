b0e3bb65e66ddf08b4ff23ac1530e2e7f6cc05b4
Alp Köksal <72392963+koksalp@users.noreply.github.com>
2023-05-01 18:56:19 +0300

    optimized search bar filtering

--- components/SearchInput.js
+++ components/SearchInput.js
@@ -1,23 +1,32 @@
-import { StyleSheet, TextInput } from "react-native";
+import { useState } from "react";
+import { StyleSheet, TextInput } from "react-native"; 
 
-// a component where user can type to 
-// filter contents through all dataset 
-// regardless of the category 
+// a component where user can type to
+// filter contents through all dataset
+// regardless of the category
 export default function SearchInput({
   handleSearch,
   resetPrograms,
   textInputRef,
-}) { 
+}) {
+  // variable that stores info about whether programs
+  // need to be reset according to the last filter selected by user
+  const [isResetNeeded, setIsResetNeeded] = useState(false);
 
-  // search through all dataset when user types and 
-  // the input field contains at least 3 letters 
-  // otherwise display contents according to 
-  // the last filter selected by user 
+  // search through all dataset when user types and
+  // the input field contains at least 3 letters
+  // otherwise display contents according to
+  // the last filter selected by user
   function handleInput(userInput) {
     if (userInput.length >= 3) {
       handleSearch(userInput);
-    } else {
+      if (!isResetNeeded) {
+        setIsResetNeeded(true);
+      }
+    } else if (isResetNeeded) {
       resetPrograms();
+      setIsResetNeeded(false);
+      console.log("reset");
     }
   }
 
